4676f2a8eefd297d812ec94484f6448661907170
huulong <n.huu.long@gmail.com>
2023-10-10 15:50:02 +0200

    [Utils] MathUtils: define vector2i_to_dominant_cardinal_direction + unit tests

--- test/unit/TestMathUtils.gd
+++ test/unit/TestMathUtils.gd
@@ -35,3 +35,63 @@ func test_cardinal_direction_to_unit_vector2i_assert_up_gives_vector_up():
 
 func test_cardinal_direction_to_unit_vector2i_assert_down_gives_vector_down():
 	assert_eq(MathUtils.cardinal_direction_to_unit_vector2i(MathEnums.CardinalDirection.DOWN), Vector2i.DOWN)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_left_gives_left():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i.LEFT), MathEnums.CardinalDirection.LEFT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_right_gives_right():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i.RIGHT), MathEnums.CardinalDirection.RIGHT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_up_gives_up():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i.UP), MathEnums.CardinalDirection.UP)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_down_gives_down():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i.DOWN), MathEnums.CardinalDirection.DOWN)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2left_1up_gives_left():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-2, -1)), MathEnums.CardinalDirection.LEFT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2left_1down_gives_left():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-2, 1)), MathEnums.CardinalDirection.LEFT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2right_1up_gives_right():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(2, -1)), MathEnums.CardinalDirection.RIGHT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2right_1down_gives_right():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(2, 1)), MathEnums.CardinalDirection.RIGHT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2up_1left_gives_up():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, -2)), MathEnums.CardinalDirection.UP)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2up_1right_gives_up():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, -2)), MathEnums.CardinalDirection.UP)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2down_1left_gives_down():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, 2)), MathEnums.CardinalDirection.DOWN)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_2down_1right_gives_down():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, 2)), MathEnums.CardinalDirection.DOWN)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1left_1up_prioritize_horizontal_gives_left():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, -1), true), MathEnums.CardinalDirection.LEFT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1left_1up_prioritize_vertical_gives_up():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, -1), false), MathEnums.CardinalDirection.UP)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1left_1down_prioritize_horizontal_gives_left():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, 1), true), MathEnums.CardinalDirection.LEFT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1left_1down_prioritize_vertical_gives_down():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(-1, 1), false), MathEnums.CardinalDirection.DOWN)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1right_1up_prioritize_horizontal_gives_right():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, -1), true), MathEnums.CardinalDirection.RIGHT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1right_1up_prioritize_vertical_gives_up():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, -1), false), MathEnums.CardinalDirection.UP)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1right_1down_prioritize_horizontal_gives_right():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, 1), true), MathEnums.CardinalDirection.RIGHT)
+
+func test_vector2i_to_dominant_cardinal_direction_assert_vector_1right_1down_prioritize_vertical_gives_down():
+	assert_eq(MathUtils.vector2i_to_dominant_cardinal_direction(Vector2i(1, 1), false), MathEnums.CardinalDirection.DOWN)
