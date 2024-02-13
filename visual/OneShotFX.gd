240a378a4b50fe832232861a03292865260d54a4
hsandt <1067772+hsandt@users.noreply.github.com>
2024-02-14 00:06:41 +0100

    [Visual] OneShotFX: add signal animation_finished and method play

--- visual/OneShotFX.gd
+++ visual/OneShotFX.gd
@@ -2,8 +2,15 @@ class_name OneShotFX
 extends Node2D
 ## Script to place on a one-shot FX represented by an AnimatedSprite2D
 ##
-## The animated sprite must have autoplay set to a non-looping animation
 ## The node will be freed after the animated sprite finished its animation
+## The animated sprite must have either:
+## - autoplay set to a non-looping animation
+## - no autoplay, but the animation to play set in inspector instead
+## In general, a OneShotFX has only one animation named "default"
+
+
+## Signal emitted when animated_sprite animation has finished
+signal animation_finished
 
 
 ## Animated sprite of which we monitor the end of animation
@@ -16,5 +23,12 @@ func _ready():
 	animated_sprite.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
 
 
+## Play the last animation (typically the one set in inspector)
+## Call this if you are not using autoplay
+func play():
+	animated_sprite.play()
+
+
 func _on_animated_sprite_2d_animation_finished():
+	animation_finished.emit()
 	queue_free()
