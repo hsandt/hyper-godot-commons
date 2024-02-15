0550c2c129ad1e85967333f829c5fb5e249b7aac
hsandt <1067772+hsandt@users.noreply.github.com>
2024-02-15 20:52:23 +0100

    [Utils] NodeUtils: instantiate methods now force readable name in editor/debug export
    ! This means node names will differ between debug and release - do not rely on node names for logic at any point, or the release build will behave unexpectedly
    ! This is slow so if spawn thousands of objects, expect an extra overhead, and you may have to add an extra flag to disable this

--- utils/NodeUtils.gd
+++ utils/NodeUtils.gd
@@ -9,23 +9,25 @@ static func queue_free_children(node: Node):
 
 
 ## Instantiate a packed scene under a parent
+## In editor and debug exports, force readable name (slow operation)
 static func instantiate_under(packed_scene: PackedScene, parent: Node) -> Node:
 	var instance := packed_scene.instantiate()
-	parent.add_child(instance)
+	parent.add_child(instance, OS.has_feature("debug"))
 	return instance
 
 
 ## Instantiate a packed scene under a parent at a global position
+## In editor and debug exports, force readable name (slow operation)
 static func instantiate_under_at(packed_scene: PackedScene, parent: Node, global_position: Vector2) -> Node:
 	var instance := packed_scene.instantiate()
-	parent.add_child(instance)
-	instance.global_position = global_position
+	parent.add_child(instance, OS.has_feature("debug"))
 	return instance
 
 
 ## Instantiate a packed scene under a parent at a global position at end of current frame
+## In editor and debug exports, force readable name (slow operation)
 static func instantiate_under_at_deferred(packed_scene: PackedScene, parent: Node, global_position: Vector2) -> Node:
 	var instance := packed_scene.instantiate()
-	parent.add_child.call_deferred(instance)
+	parent.add_child.call_deferred(instance, OS.has_feature("debug"))
 	instance.global_position = global_position
 	return instance
