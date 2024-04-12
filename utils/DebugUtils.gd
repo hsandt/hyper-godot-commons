c35644ce509b250b9c8c0012b228a613e58086a3
hsandt <1067772+hsandt@users.noreply.github.com>
2024-04-13 00:07:01 +0200

    [Utils] DebugUtils: add assert_string_member_is_not_empty and improve assert_member_is_set message

--- utils/DebugUtils.gd
+++ utils/DebugUtils.gd
@@ -4,4 +4,8 @@ class_name DebugUtils
 
 ## Assert that member is set on context
 static func assert_member_is_set(context: Node, member: Object, member_name: String):
-	assert(member, "%s: %s is not set" % [context.get_path(), member_name])
+	assert(member, "%s: member '%s' is not set" % [context.get_path(), member_name])
+
+## Assert that string member is not empty on context
+static func assert_string_member_is_not_empty(context: Node, member: String, member_name: String):
+	assert(!member.is_empty(), "%s: string member '%s' is empty" % [context.get_path(), member_name])
