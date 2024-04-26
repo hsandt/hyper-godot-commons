04a433d26fc11a2fc72ab12a4a45b6b5b7ebe6f1
hsandt <1067772+hsandt@users.noreply.github.com>
2024-04-26 19:40:01 +0200

    [OneShotFX] Assert if animation player/sprite frames use looping/ping-pong animation
    (autoplay or "default")

--- visual/OneShotFX.gd
+++ visual/OneShotFX.gd
@@ -10,11 +10,11 @@ extends Node2D
 ##
 ## Setup:
 ## 1. Prepare a non-looping default animation in AnimatedSprite2D SpriteFrames
-##    Name doesn't matter, but in case 2.B.b. below it must be &"default"
+##    Name doesn't matter in sub-cases a. below, but in sub-cases b., it must be &"default"
 ## 2. To play the animation, use the usual method:
 ##   A. with AnimatedSprite2D alone
 ##     a. set your default animation to Autoplay on Load in the SpriteFrames pane
-##     b. or call play() (it will call AnimatedSprite2D.play())
+##     b. or call play() (it will call AnimatedSprite2D.play(&"default"))
 ##   B. with AnimationPlayer controlling AnimatedSprite2D
 ##     a. set your default animation to Autoplay on Load in the Animation pane
 ##     b. or call play() (it will call AnimationPlayer.play(&"default"))
@@ -34,9 +34,31 @@ signal animation_finished
 
 func _ready():
 	if animation_player:
+		if animation_player.autoplay:
+			assert(animation_player.get_animation(animation_player.autoplay).loop_mode == Animation.LOOP_NONE,
+				"[OneShotFX] Expected autoplay animation '%s' on animation player '%s' not to loop" % [
+					animation_player.autoplay,
+					animation_player.get_path()
+					])
+		else:
+			assert(animation_player.get_animation(&"default").loop_mode == Animation.LOOP_NONE,
+				"[OneShotFX] Since not using autoplay, expected animation &'default' on animation player '%s' not to loop" %
+					animation_player.get_path())
+
 		animation_player.animation_finished.connect(_on_animation_player_animation_finished)
 	else:
 		if animated_sprite:
+			if animated_sprite.autoplay:
+				assert(not animated_sprite.sprite_frames.get_animation_loop(animated_sprite.autoplay),
+					"[OneShotFX] Expected autoplay animation '%s' on animated sprite frames '%s' not to loop" % [
+						animated_sprite.autoplay,
+						animated_sprite.sprite_frames.get_path()
+						])
+			else:
+				assert(not animated_sprite.sprite_frames.get_animation_loop(&"default"),
+					"[OneShotFX] Since not using autoplay, expected animation &'default' on animated sprite frames '%s' not to loop" %
+						animated_sprite.sprite_frames.get_path())
+
 			animated_sprite.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
 		else:
 			push_error("[OneShotFX] Neither animation_player nor animated_sprite is assigned on '%s'" % get_path())
