2b1458ae4dbadf25d345ff0169e8e98d0242c082
hsandt <1067772+hsandt@users.noreply.github.com>
2024-08-12 16:46:12 +0200

    [Visual] OneShotFX: add auto_play_from_start flag and play_from_start method
    (calling new stop, then play)
    
    New recommended workflow:
    - disable AutoPlay from your animations
    - check auto_play_from_start (default)
    
    This will auto-play one-shot FX animation from start.
    
    Existing project will have auto_play_from_start checked, but consequence is minor: it will simply be redundant with any other call to play() or auto-play setting on start. As long as a one-shot FX was not meant to be delayed (manually played later), and it shouldn't, existing projects won't be broken.
    
    Just remember to disable existing AutoPlay settings to avoid redundancy. However, AutoPlay is *still* supported to play animations other than "default", but not recommended for AnimatedSprite2D due to the risk of not playing from start (AnimationPlayer with Reset on Save is safe)

--- visual/OneShotFX.gd
+++ visual/OneShotFX.gd
@@ -14,22 +14,29 @@ extends Node2D
 ## 2. To play the animation, use the usual method:
 ##   A. with AnimatedSprite2D alone
 ##     a. set your default animation to Autoplay on Load in the SpriteFrames pane
-##     b. or call play() (it will call AnimatedSprite2D.play(&"default"))
+##     Note: make sure to reset animation after each preview to play from start
+##     b. or call play_from_start() (it will call AnimatedSprite2D.play(&"default"))
 ##   B. with AnimationPlayer controlling AnimatedSprite2D
 ##     a. set your default animation to Autoplay on Load in the Animation pane
-##     b. or call play() (it will call AnimationPlayer.play(&"default"))
+##     Note: make sure to check Reset on Save on AnimationPlayer to play from start
+##     b. or call play_from_start() (it will call AnimationPlayer.play(&"default"))
 
 
 ## Signal emitted when animated_sprite animation has finished
 signal animation_finished
 
 
-## (Optional) Animation player to monitor for end of animation
+## Animation player to monitor for end of animation
+## Either this or animated_sprite must be assigned
 @export var animation_player: AnimationPlayer
 
 ## Animated sprite of which we monitor the end of animation
 ## If the FX is compounded of multiple animated sprites, pick the longest one
+## Either this or animation_player must be assigned
 @export var animated_sprite: AnimatedSprite2D
+
+## If true, play "default" animation from start on start
+@export var auto_play_from_start: bool = true
 
 
 func _ready():
@@ -63,9 +70,18 @@ func _ready():
 		else:
 			push_error("[OneShotFX] Neither animation_player nor animated_sprite is assigned on '%s'" % get_path())
 
+	if auto_play_from_start:
+		play_from_start()
 
-## Play the last animation (typically the one set in inspector)
+
+## Play "default" animation from start
 ## Call this if you are not using autoplay
+func play_from_start():
+	stop()
+	play()
+
+
+## Play "default" animation
 func play():
 	if animation_player:
 		animation_player.play(&"default")
@@ -73,11 +89,22 @@ func play():
 		animated_sprite.play(&"default")
 
 
+## Stop the current animation
+func stop():
+	if animation_player:
+		animation_player.stop()
+	else:
+		animated_sprite.stop()
+
+
 func _on_animation_player_animation_finished(anim_name: StringName):
-	animation_finished.emit()
-	queue_free()
+	notify_animation_finished_and_queue_free()
 
 
 func _on_animated_sprite_2d_animation_finished():
+	notify_animation_finished_and_queue_free()
+
+
+func notify_animation_finished_and_queue_free():
 	animation_finished.emit()
 	queue_free()
