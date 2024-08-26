76d1f745d7ee659144f2720ff03dcbf709df5546
hsandt <1067772+hsandt@users.noreply.github.com>
2024-08-26 17:45:38 +0200

    [Utils] TimerUtils: add create_periodic_physics_timer_under and refactor create_one_shot_physics_timer_under to use it

--- utils/TimerUtils.gd
+++ utils/TimerUtils.gd
@@ -2,14 +2,13 @@ class_name TimerUtils
 ## Util static functions for Timer nodes
 
 
-## Create a one-shot physics timer with optional duration and optional callback
+## Create a periodic physics timer with optional duration and optional callback
 ## under given parent (often passing self), and return it
 ## If no duration is passed, a dummy wait_time of 1.0 is assigned, so you must
 ## make sure to always call timer.start passing a custom duration.
-static func create_one_shot_physics_timer_under(parent: Node, duration: float = 1.0,
+static func create_periodic_physics_timer_under(parent: Node, duration: float = 1.0,
 		callback: Callable = Callable()) -> Timer:
 	var timer := Timer.new()
-	timer.one_shot = true
 	timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
 	timer.wait_time = duration
 
@@ -18,4 +17,15 @@ static func create_one_shot_physics_timer_under(parent: Node, duration: float = 
 		timer.timeout.connect(callback)
 
 	parent.add_child(timer)
+	return timer
+
+
+## Create a one-shot physics timer with optional duration and optional callback
+## under given parent (often passing self), and return it
+## If no duration is passed, a dummy wait_time of 1.0 is assigned, so you must
+## make sure to always call timer.start passing a custom duration.
+static func create_one_shot_physics_timer_under(parent: Node, duration: float = 1.0,
+		callback: Callable = Callable()) -> Timer:
+	var timer := create_periodic_physics_timer_under(parent, duration, callback)
+	timer.one_shot = true
 	return timer
