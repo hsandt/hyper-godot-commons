73fb52cd913b44990d54ad28353233c2dce47175
hsandt <1067772+hsandt@users.noreply.github.com>
2024-08-30 13:15:39 +0200

    [Utils] DebugUtils: add assert_array_member_is_not_empty

--- utils/DebugUtils.gd
+++ utils/DebugUtils.gd
@@ -9,3 +9,7 @@ static func assert_member_is_set(context: Node, member: Object, member_name: Str
 ## Assert that string member is not empty on context
 static func assert_string_member_is_not_empty(context: Node, member: String, member_name: String):
 	assert(!member.is_empty(), "%s: string member '%s' is empty" % [context.get_path(), member_name])
+
+## Assert that array member is not empty on context
+static func assert_array_member_is_not_empty(context: Node, member: Array, member_name: String):
+	assert(!member.is_empty(), "%s: array member '%s' is empty" % [context.get_path(), member_name])
